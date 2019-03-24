3983d3b8f8462a9b27016116f9a7e1724796eb3e
Derek Liu <derek@derekliu.me>
2019-03-24 17:30:33 -0400

    added .gitignore, edited strend.c

--- Chpt6/strend.c
+++ Chpt6/strend.c
@@ -9,25 +9,34 @@ int custom_strend(char *s, char *t)
         {
             return 0;
         } 
-        else 
-        {
-           while (*s == *t && *s != '\0')
+       
+        while (*s == *t && *s != '\0')
         {
             s++;
             t++;
         }
-            if (*t != '\0' || *s != '\0')
+        
+        if (*t != '\0' || *s != '\0')
                 return 0;
-            else
+        else
                 return 1;
-            }
         
 }
 
 int main()
 {
-    char *a = "abcde";
+    char *a = "bde";
     char *b = "bde";
     /* printf("the return value from function is: %d\n", custom_strend(a, b)); */
-    printf("The return value from the function is %d\n", custom_strend(a, b));
+    printf("'bde, 'bde' = %d\n", custom_strend(a, b));
+
+    char *c = "this is a great day";
+    char *d = "great day";
+    /* printf("the return value from function is: %d\n", custom_strend(a, b)); */
+    printf("'this is a great day' 'great day' = %d\n", custom_strend(c, d));
+
+    char *f = "terrible result";
+    char *g = "whatever that means";
+    /* printf("the return value from function is: %d\n", custom_strend(a, b)); */
+    printf("'terrible result' 'whatever that means' = %d\n", custom_strend(f, g));
 }
